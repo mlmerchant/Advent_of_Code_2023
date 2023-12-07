60b5a1f1d778a158341bfbc8d64c8f9cf4748280
Matt <Matthew.Lee.Merchant@gmail.com>
2023-12-07 00:00:29 -0600

    Still work to do on part 2

--- Day04/Day04.Part02.sh
+++ Day04/Day04.Part02.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-INPUT_DATA="./input.txt"
+INPUT_DATA="./test_input.txt"
 
 function process_ticket_line()
 {
-    line=$1
-    line_number=${line%%:*}
-    line_number=${line_number##*[1-9]}
+    local line=$1
+    local line_number=${line%%:*}
+    line_number=${line_number##* }
     line=${line##*:}
-    card_points=0
-    winning_numbers=( $(echo $line | cut -d'|' -f1) )
-    card_numbers=( $(echo $line | cut -d'|' -f2) )
+    local card_points=0
+    local winning_numbers=( $(echo $line | cut -d'|' -f1) )
+    local card_numbers=( $(echo $line | cut -d'|' -f2) )
     for winning_number in "${winning_numbers[@]}"; do
         for card_number in "${card_numbers[@]}"; do
             if [ "$winning_number" == "$card_number" ]; then
@@ -18,30 +18,36 @@ function process_ticket_line()
             fi
         done
     done
-    
-    echo $card_points
-    
-    if [ $card_points > 0 ]; then
+
+    if (( card_points > 0 )); then
         (( total_winning_tickets++ ))
     fi
-    
-    for i in {$card_number..$((card_number + card_points))}; do
-        # echo process_ticket_line "${all_tickets[$i]}"
+
+    local -a var
+    local index=0
+    local cards=$(( line_number + card_points )) # Since array is zero indexed.
+    for (( line_number; line_number < cards; line_number++ )); do
+        var[$index]=$card_number
+        echo $index
+        (( index++ ))
+    done
+    local i
+    for i in "${var[@]}"; do
         echo "${all_tickets[$i]}"
+        process_ticket_line "${all_tickets[$i]}"
     done
 }
 
 
 total_winning_tickets=0
 
-readarray -t  all_tickets < $INPUT_DATA
+readarray -t all_tickets < $INPUT_DATA
 
 for ticket in "${all_tickets[@]}"; do
-    echo $ticket
     process_ticket_line "$ticket"
 done
 
 
 # Test input total is 30
 # Challenge input total is ??
-# echo $total_winning_tickets
+echo $total_winning_tickets
